6528369389d5885a5811d9b89df756189c8a52d4
Latifur Rahman <latifurrahman350@gmail.com>
2021-02-23 20:28:37 +0600

    done

--- desructure.js
+++ desructure.js
@@ -3,10 +3,16 @@ const person = {name: "Johny", phone: 01722457845, address: "Jahangirnagar Unive
 const gfName = person.gfName;
 const phone = person.phone;
 const friends = person.friends;
+const address = person.address;
+
+
 
 console.log(gfName, phone, friends);
 console.log(gfName, phone, friends);
+console.log(gfName, phone, address);
 console.log(gfName, phone);
 console.log(gfName, phone);
 console.log(gfName, phone);
-console.log(gfName, phone);
+
+const {gfName} = person;
+console.log(gfName);
